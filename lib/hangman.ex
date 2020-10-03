551ca896488127abcf2f17e247a1e1fa8ade08cb
SunLee128 <sunlee128@gmail.com>
2020-10-03 15:36:50 +1000

    refactor

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -2,4 +2,10 @@ defmodule Hangman do
   # This is API
   alias Hangman.Game
   defdelegate new_game(), to: Game
+  defdelegate tally(game), to: Game
+
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    {game, tally(game)}
+  end
 end
